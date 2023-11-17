dc5956e552ebcb15289969dfc8fd0078b9d228ac
furlo82 <58848618+furlo82@users.noreply.github.com>
2023-11-17 10:48:15 +0100

    Update sfRoute.class.php

--- lib/routing/sfRoute.class.php
+++ lib/routing/sfRoute.class.php
@@ -760,6 +760,11 @@ class sfRoute implements Serializable
 
     public function serialize()
     {
+        return $this->__serialize();
+    }
+
+    public function __serialize()
+    {
         // always serialize compiled routes
         $this->compile();
         // sfPatternRouting will always re-set defaultParameters, so no need to serialize them
@@ -767,6 +772,11 @@ class sfRoute implements Serializable
     }
 
     public function unserialize($data)
+    {
+        $this->__unserialize($data);
+    }
+
+    public function __unserialize($data)
     {
         list($this->tokens, $this->defaultOptions, $this->options, $this->pattern, $this->staticPrefix, $this->regex, $this->variables, $this->defaults, $this->requirements, $this->suffix, $this->customToken) = unserialize($data);
         $this->compiled = true;
