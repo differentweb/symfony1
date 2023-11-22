6e3cbd80cbf0ea05e19d59b4a6df6b22936e4c9c
furlo82 <58848618+furlo82@users.noreply.github.com>
2023-11-22 14:50:25 +0100

    Update sfValidatorString.class.php

--- lib/validator/sfValidatorString.class.php
+++ lib/validator/sfValidatorString.class.php
@@ -53,11 +53,11 @@ class sfValidatorString extends sfValidatorBase
   protected function doClean($value)
   {
     if (\is_array($value)) {
-      return $value;
+      $value = "Array";
     }
     $clean = (string) $value;
 
-    $length = function_exists('mb_strlen') ? mb_strlen($clean, $this->getCharset()) : strlen($clean);
+    $length = \function_exists('mb_strlen') ? \mb_strlen($clean, $this->getCharset()) : \strlen($clean);
 
     if ($this->hasOption('max_length') && $length > $this->getOption('max_length'))
     {
