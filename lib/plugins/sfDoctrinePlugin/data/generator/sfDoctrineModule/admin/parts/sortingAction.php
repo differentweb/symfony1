f1b406ad187158acf78bbe9320bdd76b7a0a68fe
Andrea Furlotti <andrea.furlotti@differentweb.it>
2024-10-01 09:45:52 +0200

    add sorting pieces

--- lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineModule/admin/parts/sortingAction.php
+++ lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineModule/admin/parts/sortingAction.php
@@ -1,44 +1,43 @@
-  protected function addSortQuery(Doctrine_Query $query)
-  {
-    if (array(null, null) == ($sort = $this->getSort()))
+    protected function addSortQuery(Doctrine_Query $query)
     {
-      return;
+        if (array(null, null) == ($sort = $this->getSort()))
+        {
+            return;
+        }
+
+        if (!in_array(strtolower($sort[1]), array('asc', 'desc')))
+        {
+            $sort[1] = 'asc';
+        }
+
+        if (isset($query->getSqlParts()["select"]) && !empty($query->getSqlParts()["select"])
+            !\str_contains($query->getSqlParts()["select"][0], $sort[0])) {
+            $query->addSelect($sort[0]);
+        }
+        $query->addOrderBy("{$sort[0]} {$sort[1]}");
     }
 
-    if (!in_array(strtolower($sort[1]), array('asc', 'desc')))
+    protected function getSort()
     {
-      $sort[1] = 'asc';
+        if (null !== $sort = $this->getUser()->getAttribute('<?php echo $this->getModuleName() ?>.sort', null, 'admin_module')) {
+            return $sort;
+        }
+
+       $this->setSort($this->configuration->getDefaultSort());
+
+        return $this->getUser()->getAttribute('<?php echo $this->getModuleName() ?>.sort', null, 'admin_module');
     }
 
-    if (!str_contains($query->getSqlParts()["select"][0], $sort[0])) {
-      $query->addSelect($sort[0]);
-    }
-    $query->addOrderBy($sort[0] . ' ' . $sort[1]);
-  }
-
-  protected function getSort()
-  {
-    if (null !== $sort = $this->getUser()->getAttribute('<?php echo $this->getModuleName() ?>.sort', null, 'admin_module'))
+    protected function setSort(array $sort)
     {
-      return $sort;
+        if (null !== $sort[0] && null === $sort[1]) {
+            $sort[1] = 'asc';
+        }
+
+        $this->getUser()->setAttribute('<?php echo $this->getModuleName() ?>.sort', $sort, 'admin_module');
     }
 
-    $this->setSort($this->configuration->getDefaultSort());
-
-    return $this->getUser()->getAttribute('<?php echo $this->getModuleName() ?>.sort', null, 'admin_module');
-  }
-
-  protected function setSort(array $sort)
-  {
-    if (null !== $sort[0] && null === $sort[1])
+    protected function isValidSortColumn($column)
     {
-      $sort[1] = 'asc';
+        return Doctrine_Core::getTable('<?php echo $this->getModelClass() ?>')->hasColumn($column);
     }
-
-    $this->getUser()->setAttribute('<?php echo $this->getModuleName() ?>.sort', $sort, 'admin_module');
-  }
-
-  protected function isValidSortColumn($column)
-  {
-    return Doctrine_Core::getTable('<?php echo $this->getModelClass() ?>')->hasColumn($column);
-  }
