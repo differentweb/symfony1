c30962c5c2e9679d005bff9a451bc35c6c1eac85
Andrea Furlotti <andrea.furlotti@differentweb.it>
2024-10-22 12:45:13 +0200

    HIPPO-6945 sistemazione warning

--- lib/generator/sfGenerator.class.php
+++ lib/generator/sfGenerator.class.php
@@ -18,178 +18,177 @@
  */
 abstract class sfGenerator
 {
-  protected $generatorClass = '';
-  /** @var sfGeneratorManager */
-  protected $generatorManager = null;
-  protected $generatedModuleName = '';
-  protected $theme = 'default';
-  protected $moduleName = '';
-  
-  /**
-   * Class constructor.
-   *
-   * @see initialize()
-   *
-   * @param sfGeneratorManager $generatorManager
-   */
-  public function __construct(sfGeneratorManager $generatorManager)
-  {
-    $this->initialize($generatorManager);
-  }
+    protected $generatorClass = '';
+    /** @var sfGeneratorManager */
+    protected $generatorManager = null;
+    protected $generatedModuleName = '';
+    protected $theme = 'default';
+    protected $moduleName = '';
 
-  /**
-   * Initializes the current sfGenerator instance.
-   *
-   * @param sfGeneratorManager $generatorManager A sfGeneratorManager instance
-   */
-  public function initialize(sfGeneratorManager $generatorManager)
-  {
-    $this->generatorManager = $generatorManager;
-  }
-
-  /**
-   * Generates classes and templates.
-   *
-   * @param array $params An array of parameters
-   *
-   * @return string The cache for the configuration file
-   */
-  abstract public function generate($params = array());
-
-  /**
-   * Generates PHP files for a given module name.
-   *
-   * @param string $generatedModuleName The name of module name to generate
-   * @param array  $files               A list of template files to generate
-   */
-  protected function generatePhpFiles($generatedModuleName, $files = array())
-  {
-    foreach ($files as $file)
+    /**
+     * Class constructor.
+     *
+     * @see initialize()
+     *
+     * @param sfGeneratorManager $generatorManager
+     */
+    public function __construct(sfGeneratorManager $generatorManager)
     {
-      $this->getGeneratorManager()->save($generatedModuleName.'/'.$file, $this->evalTemplate($file));
+        $this->initialize($generatorManager);
     }
-  }
 
-  /**
-   * Evaluates a template file.
-   *
-   * @param string $templateFile The template file path
-   *
-   * @return string The evaluated template
-   */
-  protected function evalTemplate($templateFile)
-  {
-    $templateFile = $this->generatorManager->getConfiguration()->getGeneratorTemplate($this->getGeneratorClass(), $this->getTheme(), $templateFile);
+    /**
+     * Initializes the current sfGenerator instance.
+     *
+     * @param sfGeneratorManager $generatorManager A sfGeneratorManager instance
+     */
+    public function initialize(sfGeneratorManager $generatorManager)
+    {
+        $this->generatorManager = $generatorManager;
+    }
 
-    // eval template file
-    ob_start();
-    require($templateFile);
-    $content = ob_get_clean();
+    /**
+     * Generates classes and templates.
+     *
+     * @param array $params An array of parameters
+     *
+     * @return string The cache for the configuration file
+     */
+    abstract public function generate($params = array());
 
-    // replace [?php and ?]
-    return $this->replacePhpMarks($content);
-  }
+    /**
+     * Generates PHP files for a given module name.
+     *
+     * @param string $generatedModuleName The name of module name to generate
+     * @param array  $files               A list of template files to generate
+     */
+    protected function generatePhpFiles($generatedModuleName, $files = array())
+    {
+        foreach ($files as $file) {
+            $this->getGeneratorManager()->save($generatedModuleName . '/' . $file, $this->evalTemplate($file));
+        }
+    }
 
-  /**
-   * Replaces PHP marks by <?php ?>.
-   *
-   * @param string $text The PHP code
-   *
-   * @return string The converted PHP code
-   */
-  protected function replacePhpMarks($text)
-  {
-    // replace [?php and ?]
-    return str_replace(array('[?php', '[?=', '?]'), array('<?php', '<?php echo', '?>'), $text);
-  }
+    /**
+     * Evaluates a template file.
+     *
+     * @param string $templateFile The template file path
+     *
+     * @return string The evaluated template
+     */
+    protected function evalTemplate($templateFile)
+    {
+        $templateFile = $this->generatorManager->getConfiguration()->getGeneratorTemplate($this->getGeneratorClass(), $this->getTheme(), $templateFile);
 
-  /**
-   * Gets the generator class.
-   *
-   * @return string The generator class
-   */
-  public function getGeneratorClass()
-  {
-    return $this->generatorClass;
-  }
+        // eval template file
+        ob_start();
+        require($templateFile);
+        $content = ob_get_clean();
 
-  /**
-   * Sets the generator class.
-   *
-   * @param string $generatorClass The generator class
-   */
-  public function setGeneratorClass($generatorClass)
-  {
-    $this->generatorClass = $generatorClass;
-  }
+        // replace [?php and ?]
+        return $this->replacePhpMarks($content);
+    }
 
-  /**
-   * Gets the sfGeneratorManager instance.
-   *
-   * @return sfGeneratorManager The sfGeneratorManager instance
-   */
-  protected function getGeneratorManager()
-  {
-    return $this->generatorManager;
-  }
+    /**
+     * Replaces PHP marks by <?php ?>.
+     *
+     * @param string $text The PHP code
+     *
+     * @return string The converted PHP code
+     */
+    protected function replacePhpMarks($text)
+    {
+        // replace [?php and ?]
+        return str_replace(array('[?php', '[?=', '?]'), array('<?php', '<?php echo', '?>'), $text);
+    }
 
-  /**
-   * Gets the module name of the generated module.
-   *
-   * @return string The module name
-   */
-  public function getGeneratedModuleName()
-  {
-    return $this->generatedModuleName;
-  }
+    /**
+     * Gets the generator class.
+     *
+     * @return string The generator class
+     */
+    public function getGeneratorClass()
+    {
+        return $this->generatorClass;
+    }
 
-  /**
-   * Sets the module name of the generated module.
-   *
-   * @param string $moduleName The module name
-   */
-  public function setGeneratedModuleName($moduleName)
-  {
-    $this->generatedModuleName = $moduleName;
-  }
+    /**
+     * Sets the generator class.
+     *
+     * @param string $generatorClass The generator class
+     */
+    public function setGeneratorClass($generatorClass)
+    {
+        $this->generatorClass = $generatorClass;
+    }
 
-  /**
-   * Gets the module name.
-   *
-   * @return string The module name
-   */
-  public function getModuleName()
-  {
-    return $this->moduleName;
-  }
+    /**
+     * Gets the sfGeneratorManager instance.
+     *
+     * @return sfGeneratorManager The sfGeneratorManager instance
+     */
+    protected function getGeneratorManager()
+    {
+        return $this->generatorManager;
+    }
 
-  /**
-   * Sets the module name.
-   *
-   * @param string $moduleName The module name
-   */
-  public function setModuleName($moduleName)
-  {
-    $this->moduleName = $moduleName;
-  }
+    /**
+     * Gets the module name of the generated module.
+     *
+     * @return string The module name
+     */
+    public function getGeneratedModuleName()
+    {
+        return $this->generatedModuleName;
+    }
 
-  /**
-   * Gets the theme name.
-   *
-   * @return string The theme name
-   */
-  public function getTheme()
-  {
-    return $this->theme;
-  }
+    /**
+     * Sets the module name of the generated module.
+     *
+     * @param string $moduleName The module name
+     */
+    public function setGeneratedModuleName($moduleName)
+    {
+        $this->generatedModuleName = $moduleName;
+    }
 
-  /**
-   * Sets the theme name.
-   *
-   * @param string $theme The theme name
-   */
-  public function setTheme($theme)
-  {
-    $this->theme = $theme;
-  }
+    /**
+     * Gets the module name.
+     *
+     * @return string The module name
+     */
+    public function getModuleName()
+    {
+        return $this->moduleName;
+    }
+
+    /**
+     * Sets the module name.
+     *
+     * @param string $moduleName The module name
+     */
+    public function setModuleName($moduleName)
+    {
+        $this->moduleName = $moduleName;
+    }
+
+    /**
+     * Gets the theme name.
+     *
+     * @return string The theme name
+     */
+    public function getTheme()
+    {
+        return $this->theme;
+    }
+
+    /**
+     * Sets the theme name.
+     *
+     * @param string $theme The theme name
+     */
+    public function setTheme($theme)
+    {
+        $this->theme = $theme;
+    }
 }
